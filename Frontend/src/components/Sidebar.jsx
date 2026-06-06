dca8b9f063458468fab08f0d1d088a3425fa8bca
Surabini Kantharaj <249471785+surabinik@users.noreply.github.com>
2026-06-06 13:34:33 +0530

    Added logout navigation

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -1,20 +1,44 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import '../Sidebar.css'
 
 function Sidebar() {
+
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    navigate('/login')
+  }
+
   return (
     <div className="sidebar">
-     <h2>
-  <Link to="/">Task Manager</Link>
-</h2>
+
+      <h2>
+        <Link to="/">Task Manager</Link>
+      </h2>
 
       <ul>
-        <li><Link to="/">Dashboard</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/tasks">Tasks</Link></li>
-        <li><Link to="/notifications">Notifications</Link></li>
-        <li><Link to="/login">Logout</Link></li>
+        <li>
+          <Link to="/">Dashboard</Link>
+        </li>
+
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+
+        <li>
+          <Link to="/tasks">Tasks</Link>
+        </li>
+
+        <li>
+          <Link to="/notifications">Notifications</Link>
+        </li>
+
+        <li onClick={handleLogout}>
+          Logout
+        </li>
+
       </ul>
+
     </div>
   )
 }
